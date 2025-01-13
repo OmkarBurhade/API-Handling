866a3899a5d50c9d78536cc23b9bd383817e1adb
burhadeomkar0603@gmail.com <burhadeomkar0603@gmail.com>
2025-01-13 14:36:49 +0530

    .env used

--- multiple-enviroment-variables/src/App.jsx
+++ multiple-enviroment-variables/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='max-w-screen-xl mx-auto h-screen bg-zinc-900'>
-      <h1 className='text-5xl'>App</h1>
+    <div className='max-w-screen-xl mx-auto h-screen bg-zinc-200 p-5'>
+      <h1 className='text-3xl font-semibold'>Title</h1>
     </div>
   )
 }
