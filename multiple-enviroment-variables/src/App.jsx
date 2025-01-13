9d9d1db0c574d9743b495193699a4ab5461eced9
burhadeomkar0603@gmail.com <burhadeomkar0603@gmail.com>
2025-01-13 14:37:14 +0530

    env updated

--- multiple-enviroment-variables/src/App.jsx
+++ multiple-enviroment-variables/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className='max-w-screen-xl mx-auto h-screen bg-zinc-200 p-5'>
       <h1 className='text-3xl font-semibold'>Title</h1>
+      <p className='text-md mt-2'>this is Env title <span className='text-purple-500 font-semibold underline'>{import.meta.VITE_Heading}The Title</span> </p>
     </div>
   )
 }
